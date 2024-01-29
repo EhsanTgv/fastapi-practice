29bbdd66adf1c00ec675c90615632ed5f540b1a4
EhsanTgv <EhsanTgv@gmail.com>
2024-01-29 09:46:12 +0300

    Implement Path Parameter

--- working.py
+++ working.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"Data": "Test"}
+
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "Regular"
+    }
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id : int = Path(description="The ID of the item you'd like to view")):
+    return inventory[item_id]
