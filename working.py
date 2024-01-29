9b0f1bd9988b48c8744be42c6ea079f1b7998c65
EhsanTgv <EhsanTgv@gmail.com>
2024-01-29 10:06:08 +0300

    Implement query parameter

--- working.py
+++ working.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"Data": "Test"}
+
 
 inventory = {
     1: {
@@ -14,6 +17,15 @@ inventory = {
     }
 }
 
+
 @app.get("/get-item/{item_id}")
-def get_item(item_id : int = Path(description="The ID of the item you'd like to view")):
+def get_item(item_id: int = Path(description="The ID of the item you'd like to view", gt=0)):
     return inventory[item_id]
+
+
+@app.get("/get-by-name")
+def get_item(*, name: Optional[str] = None, test: int):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"Data", "Not found"}
